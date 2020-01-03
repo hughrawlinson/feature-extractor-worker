5b628fa7275047603d040ba2c49b4860bfd6ff3e
Hugh Rawlinson <hughr2005@gmail.com>
2020-01-03 18:07:52 -0500

    feat(extractfeature): expose extractFeatureMainThread
    exposes a version of the feature extractor that calls the code directly, rather than in the worker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import FeatureExtractorWorker from './feature-extractor.worker.js';
+import { extractFeatures as extractFeatureMainThreadInternal } from './feature-extractor';
 
 async function askWorkerToExtractFeatures(buffers, audioFeatures, {bufferSize, hopSize, zeroPadding, windowingFunction}) {
   return new Promise((resolve, reject) => {
@@ -29,18 +30,23 @@ async function getAudioChannelDataFromBlob(audioBlob, channels) {
   return channels.map(channelIndex => audioBuffer.getChannelData(channelIndex));
 }
 
-export async function extractFeature({
-  audioBlob,
-  audioFeatures,
-  extractionParams
-}) {
-  const bufferSize = extractionParams?.bufferSize || 4096;
-  const hopSize = extractionParams?.hopSize || 0;
-  const zeroPadding = extractionParams?.zeroPadding || 0;
-  const windowingFunction = extractionParams?.windowingFunction || 'hamming';
-  const channels = extractionParams?.channels || [...Array(audioBuffer.numberOfChannels).keys()];
+function extractFeatureFunctionFactory(extractFeatureCoreFunction) {
+  return async function extractFeature({
+    audioBlob,
+    audioFeatures,
+    extractionParams
+  }) {
+    const bufferSize = extractionParams?.bufferSize || 4096;
+    const hopSize = extractionParams?.hopSize || 0;
+    const zeroPadding = extractionParams?.zeroPadding || 0;
+    const windowingFunction = extractionParams?.windowingFunction || 'hamming';
+    const channels = extractionParams?.channels || [...Array(audioBuffer.numberOfChannels).keys()];
 
-  const buffers = await getAudioChannelDataFromBlob(audioBlob, channels)
+    const buffers = await getAudioChannelDataFromBlob(audioBlob, channels)
 
-  return askWorkerToExtractFeatures(buffers, audioFeatures, {bufferSize, hopSize, zeroPadding, windowingFunction});
+    return extractFeatureCoreFunction(buffers, audioFeatures, { bufferSize, hopSize, zeroPadding, windowingFunction });
+  }
 }
+
+export let extractFeature = extractFeatureFunctionFactory(askWorkerToExtractFeatures);
+export let extractFeatureMainThread = extractFeatureFunctionFactory(extractFeatureMainThreadInternal);
